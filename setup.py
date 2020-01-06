2cc3b33dd35c13ee2400b3b643e879826be91e66
sven.weinzierl <weinzierl.sven@web.de>
2020-01-06 15:48:45 +0100

    Initial commit.

--- setup.py
+++ setup.py
@@ -9,13 +9,13 @@ with open('LICENSE') as f:
     license = f.read()
 
 setup(
-    name='canepwdl',
+    name='is-pcm',
     version='0.1.0',
     description='Next event prediction with deep learning',
     long_description=readme,
     author='Sven Weinzierl',
     author_email='sven.weinzierl@fau.de',
-    url='https://github.com/fau-is/next-event-predicton-with-deep-learning',
+    url='https://github.com/fau-is/is-pcm',
     license=license,
     packages=find_packages(exclude=('tests', 'docs'))
 )
