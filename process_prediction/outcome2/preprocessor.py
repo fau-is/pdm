66818d0aa56cab1160fcf02973d17b5d723029f6
svwe <32566119+svwe@users.noreply.github.com>
2020-05-06 00:02:31 +0200

    Update preprocessor.py

--- process_prediction/outcome2/preprocessor.py
+++ process_prediction/outcome2/preprocessor.py
@@ -226,7 +226,7 @@ class Preprocessor(object):
         Produces indices for each fold of a k-fold cross-validation.
         """
 
-        kFold = KFold(n_splits=self.data_structure['support']['num_folds'], random_state=0, shuffle=False)
+        kFold = KFold(n_splits=self.data_structure['support']['num_folds'], random_state=0, shuffle=True)
 
         for train_indices, test_indices in kFold.split(self.data_structure['data']['process_instances']):
             self.data_structure['support']['train_index_per_fold'].append(train_indices)
